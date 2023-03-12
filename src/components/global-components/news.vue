c094d6b58fdbf3265239b31825cc28da42ad956f
CegouChannel <104511385+CegouChannel@users.noreply.github.com>
2023-03-13 00:46:11 +0400

    editMain

---
+++ src/components/global-components/news.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="new justify-between">
+    <div class="image-new"></div>
+    <div class="row">
+      <h1 class="q-mt-md">{{ postSetting.date }}</h1>
+      <h2 class="q-mt-md">{{ postSetting.type }}</h2>
+    </div>
+    <p class="q-mt-md">{{ postSetting.title }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "NewBlock",
+  data() {
+    return {
+      postSetting:{
+        date:"14.01.21",
+        type:"НОВОСТИ",
+        title:"BUNYHOPE - СПИСОК ИЗМЕНЕНИЯ ОШИБОК"
+      }
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.new {
+  height: 414px;
+  max-width:500px;
+  .image-new {
+    width:100%;
+    height: 291px;
+    background-color: #d9d9d9;
+    margin-right: 30px;
+  }
+
+  h2 {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    color: #ff4655;
+  }
+
+  h1 {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    color: #383e3a;
+  }
+
+  p {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 29px;
+    color: #383e3a;
+  }
+}
+</style>
--- src/pages/main/MainPage.vue
+++ src/pages/main/MainPage.vue
@@ -1,19 +1,59 @@
 <template>
   <q-page>
-    <p>MainPage</p>
+    <div class="header q-mb-xl"></div>
+    <div class="newsContent">
+      <div class="newText full-width row  justify-between  content-start q-mb-xl">
+        <h1>ПОСЛЕДНИЕ НОВОСТИ</h1>
+        <h2>ПЕРЕЙТИ НА СТРАНИЦУ С НОВОСТИЯМИ →</h2>
+      </div>
+      <newsBlock></newsBlock>
+    </div>
   </q-page>
 </template>
 
 
 <script>
 import { defineComponent } from "vue";
+import newsBlock from "src/components/global-components/news.vue";
 export default defineComponent({
   name: "MainPage",
   data() {
     return {};
   },
-  components: {},
+  components: {
+    newsBlock,
+  },
   methods: {},
   mounted() {},
 });
 </script>
+
+<style lang="scss" scoped>
+.header {
+  background: url("../../assets/GuestPages/backgroundMain.png") no-repeat;
+  background-size: cover;
+  height: 703px;
+  width: 100%;
+}
+
+.newText {
+  h1 {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 50px;
+    line-height: 61px;
+    color: #ff4655;
+    margin:0;
+  }
+
+  h2 {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    color: #ff4655;
+  }
+}
+</style>
